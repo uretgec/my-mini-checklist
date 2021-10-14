c2acb38b3286cd63a90142748a7272a2a7848d8b
uretgec <iletisim@uretgec.com>
2021-10-14 15:18:37 +0300

    feat: build.sh file added.

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Usage: ./build service-file-name
+# Usage: ./build myminichecklist
+# Before run: chmod a+x build.sh
+
+# Local Variables
+ARGS=("$@")
+SERVICE_NAME="${ARGS[0]}"
+
+GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o "$SERVICE_NAME"
+
+if [ -f "$SERVICE_NAME" ]; then
+    echo "$SERVICE_NAME build successfully"
+
+    mv $SERVICE_NAME ./build
+    echo "$SERVICE_NAME file moved into build folder"
+    
+else
+    echo "Something wrong for building service"
+fi
+
+
